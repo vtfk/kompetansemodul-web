41bb3e0b63a2b74dd4e61150cd0d993603f0b4a9
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-09-15 09:26:53 +0200

    Lock to use given server port

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    port: 5173
+  },
   // svelte-naviagator
 	optimizeDeps: { exclude: ["svelte-navigator"] },
 })
