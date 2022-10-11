4be7118d120db8052e7d0845ce838fbc63a52594
Robin Rennesund Ellingsen <46957821+robinElli@users.noreply.github.com>
2022-10-11 11:59:35 +0200

    sjekk på lagre knappen

---
+++ src/lib/Helpers/getToday.js
@@ -0,0 +1,11 @@
+export const getToday = () => {
+    const date = new Date() 
+    const year = date.getFullYear().toString()
+    const month = date.getMonth() < 9 ? `0${(date.getMonth() + 1).toString()}` : (date.getMonth() + 1).toString() 
+    
+    return {
+        yearMonth: `${year}-${month}`,
+        year,
+        month
+    }
+}
