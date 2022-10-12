e976caec95d216281897cfbb54f5ac652df3900e
jorgtho <jorgen.thorsnes@gmail.com>
2022-10-12 08:23:07 +0200

    More Card stuff

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -8,28 +8,37 @@
     import IconHelp from './Icons/IconHelp.svelte';
     import IconCheck from './Icons/IconCheck.svelte';
     import InfoBox from './InfoBox.svelte';
+    import IconRetry from './Icons/IconRetry.svelte';
 
     // Props
     export let title = 'Ein tittel'
     export let backgroundColor = '--ecruWhite'
     export let infoBox = undefined // { content: "Det som kommer til å stå på hjelpeboksen, om du vil ha hjelpeboks" }
     export let editable = false
+    export let successTimeout = null
     export let saveFunc = async () => { // Only need if editable
         console.log('Æ lagrer')
+    }
+    export let cancelFunc = async () => { // Only need if editable
+        console.log('Æ kansellerer')
     }
 
     // State
     let isSaving = false
     let saveError = false
     let showInfoBox = false
+    let showSavedMsg = false
 
     let editInfo = get(editingPersonalia)
     editingPersonalia.subscribe(value => {
         editInfo = value
     })
 
+    const sleep = ms => new Promise(r => {successTimeout = setTimeout(r, ms)})
+
     const openEdit = () => {
         if (!editInfo.isEditing) {
+            resetSuccessMsg()
             editingPersonalia.set({ isEditing: true, editBlock: title })
         } else if (editInfo.isEditing && editInfo.editBlock !== title) {
             alert(`Du redigerer allerede ${editInfo.editBlock}, vennligst fullfør eller avbryt denne redigeringen først`)
@@ -37,16 +46,32 @@
     }
 
     const cancelEdit = () => {
+        saveError = null
+        resetSuccessMsg()
+        cancelFunc()
         editingPersonalia.set({ isEditing: false, editBlock: 'ingen' })
     }
 
+    const showSuccess = async () => {
+        showSavedMsg = true
+        await sleep(4000)
+        showSavedMsg = false
+    }
+
+    const resetSuccessMsg = () => {
+        showSavedMsg = false
+        clearTimeout(successTimeout)
+    }
+
     const saveChanges = async () => {
+        saveError = null
+        resetSuccessMsg()
         isSaving = true
         try {
             await saveFunc()
-            console.log('Detta gikk så fint så :)')
             isSaving = false
             editingPersonalia.set({ isEditing: false, editBlock: 'ingen' })
+            await showSuccess()
         } catch (error) {
             console.error('Aiaiaiai:', error)
             isSaving = false
@@ -69,10 +94,15 @@
                 {#if isSaving}
                     <IconSpinner width="2rem" />
                 {:else if saveError}
-                    {saveError}
+                    <span class="error slide fadeIn">😮 {saveError}</span>
                 {/if}
             {:else}
-                <Button buttonText="Rediger" onClick={() => openEdit()}><IconEdit slot="before" /></Button>
+                <div style="display: flex; position: relative;">
+                    {#if showSavedMsg}
+                        <div class="success slide fadeInOut">Lagret 👍</div>&nbsp&nbsp
+                    {/if}
+                    <Button buttonText="Rediger" onClick={() => openEdit()}><IconEdit slot="before" /></Button>
+                </div>
             {/if}
         {/if}
     </div>
@@ -87,16 +117,18 @@
         {#if isSaving}
             <div></div>
             <div class="saveCancel">
-                <Button buttonText="Lagrer..." disabled={true}><IconCheck slot="before" /></Button>
+                Lagrer...
+                <!--
+                <Button buttonText="Lagrer..." disabled={true}></Button>
                 &nbsp&nbsp
-                <Button buttonText="Lagrer..." disabled={true}><IconClose slot="before" /></Button>
+                <Button buttonText="Lagrer..." disabled={true}></Button>
+                -->
             </div>
         {:else if saveError}
-            <div>{saveError}</div>
+            <div></div>
             <div class="saveCancel">
-                <Button buttonText="Lagre" onClick={saveChanges}><IconCheck slot="before" /></Button>
-                &nbsp
-                &nbsp
+                <Button buttonText="Prøv igjen" onClick={saveChanges}><IconRetry slot="before" /></Button>
+                &nbsp&nbsp
                 <Button buttonText="Avbryt" onClick={cancelEdit}><IconClose slot="before" /></Button>
             </div>
         {:else}
@@ -145,4 +177,57 @@
     .saveCancel {
         display: flex;
     }
+    .error {
+        padding: 0.3rem 1rem;
+        background-color: var(--nype-2);
+    }
+    .success {
+        padding: 0.3rem 1rem;
+        background-color: var(--gress-3);
+    }
+
+    .slide {
+        padding: 0.3rem 1rem;
+        position: relative;
+        right: -5rem;
+        -webkit-animation: slide 0.5s forwards;
+        animation: slide 0.5s forwards;
+        white-space: nowrap;
+    }
+    .slide.fadeIn {
+        -webkit-animation: slide 0.5s forwards, fadein 1s forwards;
+        animation: slide 0.5s forwards, fadein 1s forwards;
+        opacity: 0;
+    }
+
+    .slide.fadeInOut {
+        -webkit-animation: slide 0.5s forwards, fadeinout 4s forwards;
+        animation: slide 0.5s forwards, fadeinout 4s forwards;
+        opacity: 0;
+    }
+
+    @-webkit-keyframes slide {
+        100% { right: 0; }
+    }
+
+    @keyframes slide {
+        100% { right: 0; }
+    }
+
+    @-webkit-keyframes fadein {
+        100% { opacity: 1; }
+    }
+
+    @keyframes fadein {
+        100% { opacity: 1; }
+    }
+
+    @-webkit-keyframes fadeinout {
+        50% { opacity: 1; }
+    }
+
+    @keyframes fadeinout {
+        50% { opacity: 1; }
+    }
+
 </style>
