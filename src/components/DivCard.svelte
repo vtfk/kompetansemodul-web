b4fcb46cece4cfa15f033ee4f1aebbd415e160c3
Robin Rennesund Ellingsen <46957821+robinElli@users.noreply.github.com>
2022-10-13 10:11:25 +0200

    emojies

--- src/components/DivCard.svelte
+++ src/components/DivCard.svelte
@@ -1,5 +1,18 @@
 <script>
     import Card from "./Card.svelte";
+    import { get } from 'svelte/store'
+    // import { saveCompetence }  from '../lib/services/useApi'
+    import { editingPersonalia } from '../lib/services/store'
+    
+    // State
+    let editInfo = get(editingPersonalia)
+    editingPersonalia.subscribe(value => {
+        editInfo = value
+    })
+
+    // Props
+    export let title = 'Litt annet'
+    export let backgroundColor = '--ecruWhite'
 
     const sleep = ms => new Promise(r => setTimeout(r, ms));
     
@@ -14,15 +27,45 @@
     }
 </script>
 
-<Card title="Litt annet" editable={true} infoBox={ {content: "Her skriver du inn noe"}} saveFunc={saveFunc} cancelFunc={cancelFunc}>
-    <div><strong>Soloroller?</strong></div>
-    <div>Nei, alle kan det</div>
-    <div><strong>Foretrukken fk</strong></div>
-    <div>Nordland</div>
-    <div><strong>Tidligere Plassering</strong></div>
-    <div>Telemark, Vestfold, kun VTFK</div>
+<Card title={title} backgroundColor={backgroundColor} editable={true} infoBox={ {content: "Her skriver du inn noe"}} saveFunc={saveFunc} cancelFunc={cancelFunc}>
+    <div>
+        {#if editInfo.isEditing && editInfo.editBlock === title}
+            <div class="soloContainer">
+                <form name="soloroleForm">
+                    <label for="solorole">Solorolle</label><br>
+                    <div class="soloRadio">
+                        <label for="ja">Ja</label>
+                        <input type="radio" id="solorole" name="solo" value="ja">
+                    </div>
+                    <div class="soloRadio">
+                        <label for="nei">Nei</label>
+                        <input type="radio" id="solorole" name="solo" value="nei">
+                    </div>
+                </form>
+            </div>
+
+        {:else}
+            <div><strong>Soloroller</strong></div>
+            <div>Nei, alle kan det</div>
+            <div><strong>Foretrukken fk</strong></div>
+            <div>Nordland</div>
+            <div><strong>Tidligere Plassering</strong></div>
+            <div>Telemark, Vestfold, kun VTFK</div>
+        {/if}
 </Card>
 
 <style>
+label {
+    font-size: 0.9em;
+    font-weight: bold;
+    font-style: italic;
+}
 
+.soloRadio {
+    padding: 0.2rem;
+}
+
+.soloContainer {
+    display: flex;
+}
 </style>
