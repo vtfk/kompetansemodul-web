d5ba13037a1952a8456405b060b1dce8138f1fcc
jorgtho <jorgen.thorsnes@gmail.com>
2022-10-19 19:58:07 +0200

    added some placeholders

--- src/Pages/Hjelp.svelte
+++ src/Pages/Hjelp.svelte
@@ -3,8 +3,16 @@
 </script>
 
 <div class="content">
-	<h1>{name}!</h1>
-	<p>Her kan du få hjelp. www.google.no </p>
+	<h1>{name}</h1>
+	<br />
+	<h2>Informasjon</h2>
+	<p>Her er litt informasjon</p>
+	<br />
+	<h2>Hvem kan se mine data?</h2>
+	<p>Her er litt informasjon om det</p>
+	<br />
+	<h2>Hva annet bør stå her?</h2>
+	<p>Hva trenger dere av hjelp?</p>
 </div>
 
 <style>
