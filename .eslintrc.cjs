762c9719aa52a1d3daa9512d728462112af1d442
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-10-25 13:03:44 +0200

    Added eslint + standard

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'plugin:svelte/recommended',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+  }
+}
