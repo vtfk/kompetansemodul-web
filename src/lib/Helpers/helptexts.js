16b6fb22e7c09c6776d7437836136a9e74e85ce0
jorgtho <jorgen.thorsnes@gmail.com>
2022-11-02 14:51:40 +0100

    added helptexts

---
+++ src/lib/Helpers/helptexts.js
@@ -0,0 +1,2 @@
+export const educationCardHelp = "<p>Informasjon om hvilken utdanning du har. Vi er interessert i enkeltemner, videreutdanninger, fagbrev eller andre grader av utdanning. Her kan du også legge inn sertifiseringer.<p>"
+    +"<br><p>Din utdanning er beskrivende for din formelle kompetanse og er dermed nødvendig i forbindelse med kartleggingen av din kompetanse.</p>"
