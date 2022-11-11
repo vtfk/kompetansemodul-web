88003895404b672f9253df224f8cadd050f0444d
Robin Rennesund Ellingsen <46957821+robinElli@users.noreply.github.com>
2022-11-11 15:25:43 +0100

    hvem kan se dette er klart

--- src/lib/Helpers/helptexts.js
+++ src/lib/Helpers/helptexts.js
@@ -2,38 +2,46 @@
 export const personaliaHelp = "<h4>Hvorfor samler vi inn din kompetanse på denne måten?</h4><p>Vi ønsker en digital oversikt over kompetansen til ansatte i fylkesadministrasjonen." 
 +"Intensjonen er å jobbe systematisk med rekruttering og kompetanseutvikling i både VTFK og de nye organisasjonene. Kompetansekartlegging er et verktøy for å anta om to nye fylkeskommuner personalmessig vil være i stand til å løse sitt samfunnsoppdrag</p> "
 +"<br><p>Husk at du også finner informasjon om kartleggingsverktøyet, GDPR og hva informasjon skal brukes til på Innsida, i personvernerklæringen og i andre informasjonskanaler. Dersom du har spørsmål eller noe er uklart er du alltid velkommen til å "
-+"sende oss en epost på <a href=mailto:delprosjektHR@vtfk.no?subject=Spørsmål-kompetanse-verktøy>delprosjektHR@vtfk.no</a></p>" 
++"sende oss en epost på <a href=mailto:delprosjektHR@vtfk.no?subject=Spørsmål-kompetanse-verktøy>delprosjektHR@vtfk.no</a></p>"
 
 // EmployeeInfoCard
 // Viser nå forsalg til ny tekst
-export const employeeInfoCardHelp = "<p>Her finner du ferdig utfylt informasjon om deg. Dette er informasjon som arbeidsgiver allerede har lagret om deg. <br><br><strong>NB!</strong> “Første ansettelses-dato“ er den datoen du for første gang ble registrert i HR-systemet, den sier nødvendigvis ikke noe om hvor lenge du har vært ansatt.</p>"
+export const employeeInfoCardHelp = "<p>Her finner du ferdig utfylt informasjon om deg. Dette er informasjon som arbeidsgiver allerede har lagret om deg. <br><br><strong>NB!</strong> “Første ansettelses-dato“ er den datoen du for første gang ble registrert i HR-systemet, den sier nødvendigvis ikke noe om hvor lenge du har vært ansatt.</p> <br> "
++"<p>Hvem kan se dette:  </p>"  
 // Gammel teskt
 // "<p>Her finner du ferdig utfylt informasjon om deg. Dette er informasjon som arbeidsgiver allerede har lagret om deg. Hvis dette ikke stemmer, kan du kontakte nærmeste leder.</p>"
 
 // PositionsCard
 export const positionsCardHelp = "<p>Informasjon om din eller dine nåværende stilling(er). Fyll inn stikkord som beskriver dine nøkkeloppgaver i dagens stilling i Vestfold – og Telemark fylkeskommune. <br><br>Hvorfor spør vi om dette? Dette trenger vi for å få en mest god oversikt over oppgaver som ligger til din stilling i dag. Det er ment å fange opp informasjon om ansatte som kan ha endret nøkkeloppgaver, jobbe i matrise eller midlertidig i prosjekter."
 +"<br><br><strong>Merk</strong>: Dersom du har en annen stilling i dag enn din hovedstilling vil begge komme opp her, fordi informasjonen hentes fra HRM-systemet. Hovedstillingen står i 0 % fordi du har permisjon fra denne. Stillingen du utfører per i dag kommer opp i stillingsprosenten du faktisk jobber i."
-+"<br><br>Noen har andre midlertidige oppgaver, som ikke er knyttet til stillingen din. For eksempel om man er deltaker i et midlertidig prosjekt.  </p>"
++"<br><br>Noen har andre midlertidige oppgaver, som ikke er knyttet til stillingen din. For eksempel om man er deltaker i et midlertidig prosjekt.  </p> <br> "
++"<p>Hvem kan se dette:  </p>"
 
 // CountySelectionCard
-export const countySelectionCardHelp = "<p>Vi ber også om at du tar stilling til foretrukken fylkeskommune. Her ønsker vi at du svarer på hvilken fylkeskommune du foretrekker (Telemark eller Vestfold). Du kan også trykke “vet ikke” om du ikke har bestemt deg eller om ønsket ditt er avhengig av andre rammebetingelser.</p><br><p><b>MERK</b>: Ditt svar er ikke bindende og er kun ment for at arbeidsgiver skal få en oversikt over hva de ansatte ønsker. Det betyr at ditt svar ikke automatisk vil få følger for innplasseringen. </p>"
+export const countySelectionCardHelp = "<p>Vi ber også om at du tar stilling til foretrukken fylkeskommune. Her ønsker vi at du svarer på hvilken fylkeskommune du foretrekker (Telemark eller Vestfold). Du kan også trykke “vet ikke” om du ikke har bestemt deg eller om ønsket ditt er avhengig av andre rammebetingelser.</p><br><p><b>MERK</b>: Ditt svar er ikke bindende og er kun ment for at arbeidsgiver skal få en oversikt over hva de ansatte ønsker. Det betyr at ditt svar ikke automatisk vil få følger for innplasseringen. </p> <br> "
++"<p>Hvem kan se dette:  </p>" 
 
 // DivCard
-export const divCardHelp = "<p><b>Eksempel 1</b>: Knut (SIK) er den eneste som jobber med planlegging etter plan- og bygningsloven som er viktig for at sektor seksjonen Samfunn og plan skal kunne gjennomføre sine lovpålagte oppgaver.<br><br><b>Eksempel 2</b>: Hjalmar (NIK) er den eneste som følger opp fylkeskommunens partnerskap og engasjement i Horizon 2020 prosjektet IMPETUS. Fra 1.1.24 vil begge fylkeskommunene måtte følge opp dette prosjektet med minst en person.<br><br><b>Eksempel 3</b>: Mona (SMM) er den eneste som er kontaktpunkt og bindeledd i kollektivtrafikken.</p>"
+export const divCardHelp = "<p><b>Eksempel 1</b>: Knut (SIK) er den eneste som jobber med planlegging etter plan- og bygningsloven som er viktig for at sektor seksjonen Samfunn og plan skal kunne gjennomføre sine lovpålagte oppgaver.<br><br><b>Eksempel 2</b>: Hjalmar (NIK) er den eneste som følger opp fylkeskommunens partnerskap og engasjement i Horizon 2020 prosjektet IMPETUS. Fra 1.1.24 vil begge fylkeskommunene måtte følge opp dette prosjektet med minst en person.<br><br><b>Eksempel 3</b>: Mona (SMM) er den eneste som er kontaktpunkt og bindeledd i kollektivtrafikken.</p> <br> "
++"<p>Hvem kan se dette:  </p>" 
 
 // WorkExperienceCard
-export const workExperienceCardHelp ="<p>Dette feltet omfatter tidligere stillinger du har hatt, dvs. ikke den stillingen du har i VTFK per i dag. Ta utgangspunkt i de siste 10-15 årene når du legger inn.</p><br><p>Hvorfor spør vi om dette? Din arbeidserfaring er èn del av din realkompetanse og er dermed nødvendig i forbindelse med kartlegging av din kompetanse.</p>"
+export const workExperienceCardHelp ="<p>Dette feltet omfatter tidligere stillinger du har hatt, dvs. ikke den stillingen du har i VTFK per i dag. Ta utgangspunkt i de siste 10-15 årene når du legger inn.</p><br><p>Hvorfor spør vi om dette? Din arbeidserfaring er èn del av din realkompetanse og er dermed nødvendig i forbindelse med kartlegging av din kompetanse.</p> <br> "
++"<p>Hvem kan se dette:  </p>" 
 
 // EducationCard
 export const educationCardHelp = "<p>Informasjon om hvilken utdanning du har. Vi er interessert i enkeltemner, videreutdanninger, fagbrev eller andre grader av utdanning."
 +"<br><br>Dersom du ikke finner riktig kategori for din utdanning kan du taste inn denne manuelt. Du må ikke velge fra den forhåndsbestemte listen. "    
-+"<br><br>Din utdanning er beskrivende for din formelle kompetanse og er dermed nødvendig i forbindelse med kartleggingen av din kompetanse.</p>"
++"<br><br>Din utdanning er beskrivende for din formelle kompetanse og er dermed nødvendig i forbindelse med kartleggingen av din kompetanse.</p> <br> "
++"<p>Hvem kan se dette:  </p>" 
 
 // CertificationCard
-export const certificationCardHelp = "<p>Kurs og sertifiseringer hvor du har tilegnet deg kompetanse som er relevant.</p>"
+export const certificationCardHelp = "<p>Kurs og sertifiseringer hvor du har tilegnet deg kompetanse som er relevant.</p> <br> "
++"<p>Hvem kan se dette:  </p>" 
 
 // ExperienceCard
-export const experienceCardHelp = "<p>Verv hvor du har tilegnet deg kompetanse som er relevant. </p>"
+export const experienceCardHelp = "<p>Verv hvor du har tilegnet deg kompetanse som er relevant. </p> <br> "
++"<p>Hvem kan se dette:  </p>" 
 
 // HelpPage
 // Informasjon 
