3736874e9c577017cdca06353f3122e6b487c29a
Robin Rennesund Ellingsen <46957821+robinElli@users.noreply.github.com>
2022-11-14 22:00:59 +0100

    skeleton mandatory check

--- src/Pages/Personalia.svelte
+++ src/Pages/Personalia.svelte
@@ -14,8 +14,9 @@
 	import EmployeeInfoCard from '../components/EmployeeInfoCard.svelte';
 	import CountySelectionCard from '../components/CountySelectionCard.svelte'
 	import CertificationCard from '../components/CertificationCard.svelte'
-	import { personaliaHelp } from '../lib/Helpers/helptexts'
+	import { countySelectionCardHelp, personaliaHelp } from '../lib/Helpers/helptexts'
 	import { changePage } from '../lib/Helpers/changePage'
+  import { each, loop_guard } from 'svelte/internal';
 
 	// State
 	let showInfoBox = false
@@ -52,6 +53,23 @@
 				<InfoBox content={personaliaHelp} html={true} open={showInfoBox} onClose={() => {showInfoBox = !showInfoBox}} />
 			{:else}
 				<h2>Redigerer {editInfo.editBlock}</h2>
+			{/if}
+		</div>
+		<div>
+			{#if res.mandatoryCompetenceInput}
+				{#each res.competenceData.positionTasks as pos}
+					{#if pos.tasks.length <= 0}
+						<p>Stillingen din i {pos.positionParent} har ingen nøkkelopppaver, dette er obligatorisk å fylle ut!</p>
+					{/if}
+				{/each}
+				{#if !res.competenceData.perfCounty}
+					<p>Du må velge et valg fra nedtrekksmenyen i Ønsket fylkeskommune / arbeidssted etter oppdeling!</p>
+				{/if}
+				{#if !res.competenceData.other?.soloRole}
+					<p>Du må si om du har noen kritiske oppgaver eller ikke!</p>
+				{/if}
+			{:else if !res.mandatoryCompetenceInput}
+				<p>Det er ikke obligatorisk for deg å fylle ut her - men du er hjertelig velkommen til å fylle ut det du ønsker</p>
 			{/if}
 		</div>
 		<!--<p style="color: var(--deepSeaGreen);"><strong>Til dagens seksjonsledermøte:</strong> Klikk her: <a href ="mailto:jorgen.thorsnes@vtfk.no;robin.ellingsen@vtfk.no?subject=Tilbakemelding på kompetanse-verktøy"> Lag tilbakemeldings-epost.</a> Skriv inn tilbakemeldinger i e-posten som åpnes, og send når du er ferdig å teste</p><br/> -->
